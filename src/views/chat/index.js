cf0f7e3856a565e33416abc30509968d0b7fbf75
p0sidonz <ethicalankitx0@gmail.com>
2023-08-02 21:27:45 +0530

    Update index.js

--- src/views/chat/index.js
+++ src/views/chat/index.js
@@ -69,7 +69,7 @@ const ChatMainPage = () => {
     const user = JSON.parse(window.localStorage.getItem('user'));
     const [lastIndexOf, setLastIndexOf] = useState(-1);
     const [isLoading, setIsLoading] = useState(Boolean(false));
-    const socket = io('https://env-0216910.cloudjiffy.net');
+    const socket = io('http://env-2589.cloudjiffy.net');
     // const socket = io('http://localhost:3000/');
 
     const theme = useTheme();
